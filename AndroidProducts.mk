fcff8c0c9dc26f9c062b73ef3794ef0c5c6cf400
conradhale <76891485+conradhale@users.noreply.github.com>
2021-01-31 15:17:59 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_davinci.mk
+    $(LOCAL_DIR)/aex_davinci.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_davinci-user \
-    ancient_davinci-userdebug \
-    ancient_davinci-eng
+    aex_davinci-user \
+    aex_davinci-userdebug \
+    aex_davinci-eng
