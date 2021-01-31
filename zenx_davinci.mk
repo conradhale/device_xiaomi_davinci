8a1e05dcfc56c0685ed5e9fe8f7fb7457892fd40
conradhale <conradhale12@gmail.com>
2021-01-31 12:20:45 -0500

    initial

---
+++ zenx_davinci.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from davinci device
+$(call inherit-product, device/xiaomi/davinci/device.mk)
+
+# Inherit some common ZenX-OS stuff.
+$(call inherit-product, vendor/zenx/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := zenx_davinci
+PRODUCT_DEVICE := davinci
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 9T
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
